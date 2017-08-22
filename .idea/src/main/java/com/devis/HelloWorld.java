a750a909891dea25a22bf51067f8709d8d22d727
Devis Balsemin <devis.balsemin@flexvalley.com>
2017-08-22 07:22:44 +0200

    First project

---
+++ .idea/src/main/java/com/devis/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.devis;
+
+/**
+ * Created by Devis Balsemin on 22/08/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+    }
+}
